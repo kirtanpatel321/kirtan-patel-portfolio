220b394653e9530ad5dcf5013443480c1ed35044
Kirtan Patel <kirtanpatel2201@gmail.com>
2026-03-05 19:28:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 const text = "Specializing in secure software systems, cryptography, and resilient architecture design.";
+
 const typingElement = document.querySelector(".typing-text");
 
 let index = 0;
 
-function typeText() {
-    if (index < text.length) {
-        typingElement.textContent += text.charAt(index);
-        index++;
-        setTimeout(typeText, 80);  // slower typing speed
-    }
+function typeText(){
+if(index < text.length){
+typingElement.textContent += text.charAt(index);
+index++;
+setTimeout(typeText,80);
+}
 }
 
-window.addEventListener("load", typeText);
+window.addEventListener("load",typeText);
+
+
+/* SCROLL REVEAL */
+
+function revealSections(){
+
+const reveals=document.querySelectorAll(".reveal");
+
+for(let i=0;i<reveals.length;i++){
+
+let windowHeight=window.innerHeight;
+let elementTop=reveals[i].getBoundingClientRect().top;
+
+let revealPoint=120;
+
+if(elementTop < windowHeight - revealPoint){
+reveals[i].classList.add("active");
+}
+
+}
+
+}
+
+window.addEventListener("scroll",revealSections);
